265d8bffa5251ded51083fe0ec204e841064828e
Syam Joshi Kalaga <syamjoshik@gmail.com>
2026-05-06 00:37:27 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "joshi.k8s.dev" # Replace with your actual S3 bucket name
+    key    = "githubactions/terraform.tfstate"
+    region = "us-east-1"
   }
 }
